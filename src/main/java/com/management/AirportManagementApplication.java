9815feded57ce3241971fe5254d9313740c009c3
= <tigran_grigoryan05@inbox.ru>
2023-05-08 21:31:37 +0400

    Upload App

---
+++ src/main/java/com/management/AirportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/AirportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
